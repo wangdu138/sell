054f670219aa830cb746f478c3ef103c114b806b
Luke Wang <Luke.Wang@morningstar.com>
2017-08-21 15:07:00 +0800

    Show seller info in header

---
+++ src/components/ratings/ratings.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    I am ragints
+  </div>
+</template>
+
+<script>
+  export default {};
+</script>
+
+<style>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,13 +1,38 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import VueRouter from 'vue-router';
+import VueResource from 'vue-resource';
 import App from './App';
+import goods from 'components/goods/goods';
+import ratings from 'components/ratings/ratings';
+import seller from 'components/seller/seller';
+
+import './common/stylus/index.styl';
 
 Vue.config.productionTip = false;
+
+Vue.use(VueRouter);
+Vue.use(VueResource);
+
+// define some routes
+const routes = [
+    { path: '/', redirect: '/goods' },
+    { path: '/goods', component: goods },
+    { path: '/ratings', component: ratings },
+    { path: '/seller', component: seller }
+];
+
+// create router instance
+const router = new VueRouter({
+    linkActiveClass: 'active',
+    routes
+});
 
 /* eslint-disable no-new */
 new Vue({
     el: '#app',
     template: '<App/>',
-    components: { App }
+    components: { App },
+    router
 });
